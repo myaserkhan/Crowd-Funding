ca25a3c1bbbee8afd213e85c80ae00bed8ef12f4
Yasir Khan <khanyaser007@gmail.com>
2023-05-30 15:01:52 +0500

    fix: Update createCampaign function arguments

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -6,22 +6,22 @@ import { ethers } from 'ethers';
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  const { contract } = useContract('0xa8aB6dCB526a99831147d0d97c3DfA38d461b5F8');
-  const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
+  const { contract } = useContract("0xa8aB6dCB526a99831147d0d97c3DfA38d461b5F8");
+  const { mutateAsync: createCampaign } = useContractWrite(contract, "createCampaign");
 
   const address = useAddress();
   const connect = useMetamask();
 
   const publishCampaign = async (form) => {
     try {
-      const data = await createCampaign([
+      const data = await createCampaign({ args: [
         address, // owner
         form.title, // title
         form.description, // description
         form.target,
         new Date(form.deadline).getTime(), // deadline,
         form.image
-      ])
+      ]});
 
       console.log("contract call success", data)
     } catch (error) {
